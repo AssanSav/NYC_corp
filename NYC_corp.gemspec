3b39ee427537d402084198673de8553519a68331
Assane Savadogo <assane.savadogo81@gmail.com>
2019-09-10 23:59:08 -0400

    thrird commit

--- NYC_corp.gemspec
+++ NYC_corp.gemspec
@@ -9,7 +9,7 @@ Gem::Specification.new do |spec|
   spec.email         = ["assane.savadogo81@gmail.com"]
 
   spec.summary       = %q{NYC 50 Highly Rated Companies}
-  spec.description   = %q{Listing of: Company Name to their Ratings Summaries and Links in NYC}
+  spec.description   = %q{Listing off: Company Names to their Ratings, Summaries and Links in NYC}
   spec.homepage      = "https://github.com/AssanSav"
   spec.license       = "MIT"
 
@@ -25,7 +25,7 @@ Gem::Specification.new do |spec|
     `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
   end
   #spec.bindir        = "exe"
-  spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
+  spec.executables   = ["corp_menu"]
   spec.require_paths = ["lib"]
 
   spec.add_development_dependency "bundler", "~> 2.0"
--- lib/NYC_corp/cli.rb
+++ lib/NYC_corp/cli.rb
@@ -1,49 +1,53 @@
 class  NYCCorp::CLI 
 
     def call 
-        puts "Welcome to NYC 50 Best rated Coporations".colorize(:green)
-        input = nil 
+            input = nil 
         until input == "exit" 
-            puts "Type 'corp' to display companies".colorize(:yellow)
-            puts "Type 'menu' to dysplay all Companies".colorize(:yellow)
+            puts "\nWelcome to NYC 50 Best Highly rated CORPORATIONS".colorize(:blue)
+            puts "Type 'MENU' to display companies".colorize(:yellow)
+            puts "Type 'ALL' to dysplay all Companies".colorize(:yellow)
+            puts "Type 'NUMB' to choose between 1 & 50".colorize(:yellow)
             
-       
-        input = gets.strip.downcase
+            input = gets.strip.downcase
         case input 
-            when "corp"
-                listBy_name
             when "menu"
+                listBy_name
+            when "all"
                 listAll_Corp
-            when "number"
+            when "numb"
                 listBy_number
             when "exit"
-            puts "GOODBYE"
+            puts "GOODBYE & THANKS FOR YOUR BUSINESS!".colorize(:blue)
                  exit
             else 
-                puts "WOOPS! invalid input Type corp or Number First or 'exit' to exit".colorize(:green)
+            puts "WOOPS! Didin't understand!!!".colorize(:green)
+            call
             end
         end
     end 
+
 
     def listBy_name
         NYCCorp::Company.all.each.with_index(1) do |corp, index|
             puts "#{index}. #{corp.name.colorize(:blue)}"
         end
-       listBy_number
+        listBy_number
     end
+
 
     def listAll_Corp
         NYCCorp::Company.all.each.with_index(1) do |corp, index|
             puts "#{index}. #{corp.name.colorize(:blue)}"
-            puts "Domaine: #{corp.domaine.colorize(:red)}" 
+            puts "Domaine:#{corp.domaine.colorize(:red)}" 
             puts "Rating: #{corp.rating.colorize(:red)}" 
             puts "Summary: #{corp.summary}"
             puts "Link: #{corp.link.colorize(:red)}\n\n"
         end
     end
 
+
     def listBy_number
-        puts "Enter 1 to 50".colorize(:green)  
+        puts "\nEnter 1 to 50".colorize(:green)  
         input = gets.strip.to_i
     if (1..NYCCorp::Company.all.size).include?(input) && input <= NYCCorp::Company.all.size && input != nil
         selection = NYCCorp::Company.all.each {|corp| corp}[input - 1]
@@ -52,10 +56,9 @@ class  NYCCorp::CLI
         puts "Rating: #{selection.rating.colorize(:red)}"  
         puts "Summary: #{selection.summary.colorize(:red)}"
         puts "Link: #{selection.link.colorize(:red)}"
-    else 
+        else 
         listBy_number
         end
     end
 
 end
-
