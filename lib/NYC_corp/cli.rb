638a1315ff329e5dfc8e85ea89a818377b72eac1
Assane Savadogo <assane.savadogo81@gmail.com>
2019-09-11 12:18:26 -0400

    fourth commit

--- lib/NYC_corp/cli.rb
+++ lib/NYC_corp/cli.rb
@@ -1,26 +1,24 @@
 class  NYCCorp::CLI 
 
     def call 
-            input = nil 
-        until input == "exit" 
+            @input = nil 
+        until @input == "exit" 
             puts "\nWelcome to NYC 50 Best Highly rated CORPORATIONS".colorize(:blue)
             puts "Type 'MENU' to display companies".colorize(:yellow)
             puts "Type 'ALL' to dysplay all Companies".colorize(:yellow)
-            puts "Type 'NUMB' to choose between 1 & 50".colorize(:yellow)
+            puts "Type 'EXIT' to exit".colorize(:yellow)
             
-            input = gets.strip.downcase
-        case input 
+            @input = gets.strip.downcase
+        case @input 
             when "menu"
                 listBy_name
             when "all"
                 listAll_Corp
-            when "numb"
-                listBy_number
             when "exit"
             puts "GOODBYE & THANKS FOR YOUR BUSINESS!".colorize(:blue)
                  exit
             else 
-            puts "WOOPS! Didin't understand!!!".colorize(:green)
+            puts "WOOPS! Didin't understand!!!".colorize(:red)
             call
             end
         end
@@ -47,18 +45,28 @@ class  NYCCorp::CLI
 
 
     def listBy_number
-        puts "\nEnter 1 to 50".colorize(:green)  
-        input = gets.strip.to_i
-    if (1..NYCCorp::Company.all.size).include?(input) && input <= NYCCorp::Company.all.size && input != nil
-        selection = NYCCorp::Company.all.each {|corp| corp}[input - 1]
+        puts "\nEnter 1 to 50 or Exit to MENU".colorize(:green) 
+        @input = gets.strip
+        choice = @input.to_i
+    if (1..NYCCorp::Company.all.size).include?(choice) && choice <= NYCCorp::Company.all.size && @choice != "exit"
+        selection = NYCCorp::Company.all.each {|corp| corp}[choice- 1]
         puts "Name: #{selection.name.colorize(:red)}"
         puts "Domaine: #{selection.domaine.colorize(:red)}"
         puts "Rating: #{selection.rating.colorize(:red)}"  
         puts "Summary: #{selection.summary.colorize(:red)}"
         puts "Link: #{selection.link.colorize(:red)}"
+        checker
         else 
-        listBy_number
+            puts "WOOPS! Wrong TYPO...".colorize(:red)
+        call
         end
+    end
+
+    def checker 
+        #puts "What would you like to see"
+        listBy_number
+        listBy_name
+        listAll_Corp
     end
 
 end
